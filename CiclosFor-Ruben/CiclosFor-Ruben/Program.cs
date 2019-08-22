36546dfec5983292a265467bdb6c2c6025d278ec
oscar eduardo melo <omelo@ucol.mx>
2019-08-21 20:13:09 -0500

    Se modificó el códifo de C#

---
+++ CiclosFor-Ruben/CiclosFor-Ruben/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CiclosFor_Ruben
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z;
+            Console.WriteLine("Imprime los numeros del x hasta y de -1");
+            Console.WriteLine("empezamos el ciclo");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("terminamos el ciclo");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("de cuanto en cunto?");
+            z = Convert.ToDouble(Console.ReadLine());
+
+            if (x < y)
+            {
+                for (double i = x; i < y; i += z)
+                {
+                    Console.WriteLine("i= " + i);
+                }
+            }
+        }
+
+    }
+}
